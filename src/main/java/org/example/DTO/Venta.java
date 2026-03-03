30bc039b1e166d46e4aba171638f8d64794ecded
Sebastian Mahecha <juans-mahechac@unilibre.edu.co>
2026-03-02 20:19:09 -0500

    Añadir lombok a Comprador y Venta

--- src/main/java/org/example/DTO/Venta.java
+++ src/main/java/org/example/DTO/Venta.java
@@ -1,7 +1,12 @@
 package org.example.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+@Getter
+@Setter
 
 public class Venta {
     private Evento evento;
