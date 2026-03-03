9d3cfd54ae99385c1b70a43607f8540d3e7de011
Sebastian Mahecha <juans-mahechac@unilibre.edu.co>
2026-03-02 21:15:43 -0500

    Ventanas hechas: Eventos, Principal, Registrar

--- src/main/java/org/example/Ventana/VentanaEventos.java
+++ src/main/java/org/example/Ventana/VentanaEventos.java
@@ -1,4 +1,45 @@
 package org.example.Ventana;
 
-public class VentanaEventos {
+import org.example.DTO.Evento;
+import org.example.Gestion.GestionEvento;
+import org.example.Gestion.GestionVenta;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaEventos extends JFrame {
+
+    public VentanaEventos(GestionEvento gestionEvento, GestionVenta gestionVenta) {
+
+        setTitle("Eventos disponibles");
+        setSize(500,400);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel(new BorderLayout(10,10));
+        panel.setBorder(BorderFactory.createEmptyBorder(15,15,15,15));
+
+        JList<Evento> listaEventos = new JList<Evento>(
+                gestionEvento.listarEventos().toArray(new Evento[0])
+        );
+
+        JScrollPane scroll = new JScrollPane(listaEventos);
+
+        JButton Comprar = new JButton("Comprar Boletas");
+        Comprar.setBackground(new Color(40,167,69));
+        Comprar.setForeground(Color.white);
+
+        Comprar.addActionListener(e -> {
+            Evento sel = listaEventos.getSelectedValue();
+            if (sel != null) {
+                //new VentanaCompra(sel,gestionVenta).setVisible(true);
+            }
+        });
+
+        panel.add(scroll, BorderLayout.CENTER);
+        panel.add(Comprar, BorderLayout.SOUTH);
+
+        add(panel);
+
+
+    }
 }
