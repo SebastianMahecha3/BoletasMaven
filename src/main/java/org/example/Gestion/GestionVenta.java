d9dc1a7b43bcaa455f6b9056640c18312fbccc63
Sebastian Mahecha <juans-mahechac@unilibre.edu.co>
2026-03-02 20:26:23 -0500

    Corrección gestion Venta

--- src/main/java/org/example/Gestion/GestionVenta.java
+++ src/main/java/org/example/Gestion/GestionVenta.java
@@ -13,8 +13,16 @@ public class GestionVenta {
 
     public static final int MaxBoletasPorComprador = 10;
 
+    public GestionVenta(){
+        ventas = new ArrayList<>();
+    }
+
     public boolean registrarVenta(Evento evento, Localidad localidad, Comprador comprador, int cantidad, TipoPago tipoPago) {
-        if(cantidad > MaxBoletasPorComprador){
+        if(cantidad <=0){
+            return false;
+        }
+        int totalActual = totalPorPersona(evento,comprador.getIdentificacion());
+        if(totalActual+cantidad > MaxBoletasPorComprador){
             return false;
         }
         if(!FechaHoraUtil.ventaPermitida(evento)){
@@ -26,5 +34,17 @@ public class GestionVenta {
         Venta venta = new Venta(evento,localidad,comprador,cantidad, LocalDateTime.now(),tipoPago);
         ventas.add(venta);
         return true;
+    }
+    private int totalPorPersona(Evento evento, String identificacion){
+        int total = 0;
+        for(Venta venta : ventas){
+            if(venta.getEvento().equals(evento) && venta.getComprador().getIdentificacion().equals(identificacion)){
+                total+=venta.getCantidad();
+            }
+        }
+        return total;
+    }
+    public List<Venta> getVentas(){
+        return ventas;
     }
 }
